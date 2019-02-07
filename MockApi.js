135108fe74ad7ebf7d3f6a64e2cb81d2fe1d5e43
berkin tatlisu <barron9@yandex.com>
2019-02-07 20:50:17 +0300

    Create MockApi.js

---
+++ MockApi.js
@@ -0,0 +1,24 @@
+export default {
+  _getlanguages: (id: string) => {
+    return {
+      "status": 200,
+      "data": {
+        "langs": [
+
+          { "name": "Türkçe", "isChecked": true },
+          { "name": "İngilizce", "isChecked": false }, { "name": "Türkçe", "isChecked": false },
+          { "name": "İngilizce", "isChecked": false }, { "name": "Türkçe", "isChecked": false },
+          { "name": "İngilizce", "isChecked": false }, { "name": "Türkçe", "isChecked": false },
+          { "name": "İngilizce", "isChecked": false }, { "name": "Türkçe", "isChecked": false },
+          { "name": "İngilizce", "isChecked": false }, { "name": "Türkçe", "isChecked": false },
+          { "name": "İngilizce", "isChecked": false }, { "name": "Türkçe", "isChecked": false },
+          { "name": "İngilizce", "isChecked": false }, { "name": "Türkçe", "isChecked": false },
+          { "name": "İngilizce", "isChecked": false }, { "name": "Türkçe", "isChecked": false },
+          { "name": "İngilizce", "isChecked": false }
+
+        ]
+
+      }
+    }
+  }
+  }
